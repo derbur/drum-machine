41d34238f84c0201c72ff33698a71b8582aeafb1
Derrick Burns <derrick@derricks-air.lan>
2020-11-26 18:43:39 -0500

    drummin something up

---
+++ src/components/Pad/Pad.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let soundSrc: string;
+  export let soundName: string;
+  export let triggerKey: string;
+  let audio: HTMLAudioElement;
+
+  function playSound() {
+    if(!audio.paused) {
+      audio.currentTime = 0;
+    }
+    audio.play();
+  }
+  
+  function handleKeydown(e) {
+    console.log(e.key);
+    if(e.key.toLowerCase() === triggerKey ) {
+      playSound();
+    }
+  }
+</script>
+
+<style>
+  /* .pad {
+    width: 200px;
+    height: 200px;
+    background: rgba(0,0,0,0.5);
+    display: inline-block;
+    margin: 7.5px 5px;
+    box-shadow: 0 4px 0 #1e1e1e, 0px 0px 10px 1px rgba(0,0,0,0.5);
+    border: 1px solid #1e1e1e;
+    border-radius: 3px;
+  } */
+</style>
+
+<svelte:window on:keydown={handleKeydown} />
+<button class="pad" on:click={playSound}>{ soundName }</button>
+<audio bind:this={audio} src={soundSrc}>
+  <track kind="captions" />
+</audio>
---
+++ src/components/PadSet/PadSet.svelte
@@ -0,0 +1,33 @@
+<script lang='ts'>
+  import Pad from '../Pad/Pad.svelte';
+
+  interface Sound {
+    trigger: string,
+    source: string
+  }
+
+  const soundList: Sound[] = [ 
+                      { trigger: 'q', source: 'Clap1.wav' },
+                      { trigger: 'w', source: 'Hats2.wav' },
+                      { trigger: 'e', source: 'HH2.wav' },
+                      { trigger: 'r', source: 'Kik12.wav' },
+                      { trigger: 't', source: 'OH21.wav' },
+                      { trigger: 'y', source: 'Snare12.wav' }
+                    ];
+</script>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+    grid-template-rows: 50% 50%; 
+    column-gap: 10px;
+    row-gap: 15px;
+  }
+</style>
+
+<div class="container">
+  { #each soundList as sound }
+    <Pad soundName={sound.source} soundSrc={`samples/kit-1/${sound.source}`} triggerKey={sound.trigger} />
+  { /each }
+</div>
